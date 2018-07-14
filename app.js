e5f9cc57b229a4f592ee8c84e40d426ac39ee114
Kingston Tam <web@ktam.org>
2018-07-14 23:54:17 +0300

    Update to latest version of express

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
  */
 
 var express = require('express');
-var exphbs = require('express3-handlebars');
+var exphbs = require('express-handlebars');
+var logger = require('morgan');
+var errorHandler = require('errorhandler')
 var routes = require('./routes');
 var classroutes = require('./routes/class');
 var http = require('http');
@@ -17,26 +19,16 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
-
 app.get('/:className/:lessonName?', classroutes.showClass);
 
-app.get('/', routes.index);
+// development only
+if ('development' === app.get('env')) {
+  app.use(errorHandler());
+}
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
